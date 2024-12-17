45cdfb71f0189d943e437de87a71c2bb2b53081a
TimotheeAllix <timothee.allix@gmail.com>
2024-12-17 11:18:25 +0100

    Test encore

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,4 +8,19 @@ public class Calculator {
     public int addition(int a, int b) {
         return a + b;
     }
+
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division par zéro impossible");
+        }
+        return (double) a / b;
+    }
 }
