12ff72aca68de009d7b73a2dca2eaa5865587ea4
TimotheeAllix <timothee.allix@gmail.com>
2024-12-17 11:50:55 +0100

    Test encore

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,29 +8,29 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
-    public void testAddition1() {
+     void testAddition1() {
         Calculator calc = new Calculator();
         assertEquals(5, calc.addition(2, 3));
     }
     @Test
-    public void testSoustraction() {
+     void testSoustraction() {
         Calculator calc = new Calculator();
         assertEquals(1, calc.soustraction(3, 2));
     }
 
     @Test
-    public void testMultiplication() {
+     void testMultiplication() {
         Calculator calc = new Calculator();
         assertEquals(6, calc.multiplication(2, 3));
     }
 
     @Test
-    public void testDivision() {
+     void testDivision() {
         Calculator calc = new Calculator();
         assertEquals(2.0, calc.division(6, 3));
     }
     @Test
-    public void testDivision2(){
+    void testDivision2(){
         Calculator calc = new Calculator();
         assertThrows(IllegalArgumentException.class, () -> calc.division(1, 0));
     }
